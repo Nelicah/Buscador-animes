6e01a8c537ec8f1d3446694efd9efa2947cc167c
Nelicah <candezmr2@gmail.com>
2025-05-19 18:09:59 +0200

    render ul in JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,27 @@
 "use strict";
 
-//DATOS
 const searchInput = document.querySelector(".js_searchInput");
 const serachButton = document.querySelector(".js_seachButton");
+const animeList = document.querySelector(".js_animeList");
+//DATOS
+
+const oneAnime = {
+  title: "Naruto",
+  image: "https://cdn.myanimelist.net/images/anime/1141/142503.jpg",
+  alt: "Naruto",
+};
+
+//FUNCIONES
+function renderOneAnime(oneAnime) {
+  //renderizamos desde JS el html
+  const html = `<li>
+      <img 
+      src=${oneAnime.image}
+      alt=${oneAnime.alt}
+      />
+      <p> ${oneAnime.title} </p>
+    </li>`;
+
+  animeList.innerHTML += html;
+}
+renderOneAnime(oneAnime);
