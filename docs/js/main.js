0011e9becb535624065d618a24b6f29ad0538f03
nely <candezmr2@gmail.com>
2025-07-14 15:51:28 +0200

    update docs

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,30 +1,37 @@
-const h=document.querySelector(".js_searchInput"),I=document.querySelector(".js_searchButton"),L=document.querySelector(".js_animeList"),y=document.querySelector(".js_resultTitle"),g=document.querySelector(".js_favSection"),C=document.querySelector(".js_resetButton"),T=document.createTextNode("Resultados");y.appendChild(T);const _=document.createElement("h2"),F=document.createTextNode("Series Favoritas"),r=document.createElement("ul");_.appendChild(F);g.appendChild(_);g.appendChild(r);let o=[],l=[];function q(e){const t=l.findIndex(i=>i.mal_id===e.mal_id);return e.images.jpg.image_url==="https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"?`<li class="li-anime js_liAnime" data-hook=${e.mal_id}>
+const r=document.querySelector(".js_searchInput"),j=document.querySelector(".js_searchButton"),S=document.querySelector(".js_animeList"),I=document.querySelector(".js_resultTitle"),h=document.querySelector(".js_favSection"),L=document.querySelector(".js_resetButton"),y=document.createTextNode("Resultados");I.appendChild(y);const f=document.createElement("h2"),C=document.createTextNode("Series Favoritas"),o=document.createElement("ul");f.appendChild(C);h.appendChild(f);h.appendChild(o);let d=[],i=[];function T(t){const e=i.findIndex(l=>l.mal_id===t.mal_id);return t.title_english===null?`<li class="li-anime js_liAnime" data-hook=${t.mal_id}>
+      <img class="anime-picture" 
+      src=${t.images.jpg.image_url}
+      alt="${t.title}"
+      />
+      <p class="p1"> - ${t.title} </p>
+      <p class="p2"> - ${t.title} </p>
+    </li>`:t.images.jpg.image_url==="https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"?`<li class="li-anime js_liAnime" data-hook=${t.mal_id}>
       <img class="anime-picture" 
       src="https://placehold.co/210x300/ffffff/555555?text=TV"
-      alt="${e.title}"
+      alt="${t.title}"
       />
-      <p class="p1"> - ${e.title_english} </p>
-      <p class="p2"> - ${e.title} </p>
-    </li>`:t===-1?`<li class="li-anime js_liAnime" data-hook=${e.mal_id}>
+      <p class="p1"> - ${t.title_english} </p>
+      <p class="p2"> - ${t.title} </p>
+    </li>`:e===-1?`<li class="li-anime js_liAnime" data-hook=${t.mal_id}>
       <img class="anime-picture" 
-      src=${e.images.jpg.image_url}
-      alt="${e.title}"
+      src=${t.images.jpg.image_url}
+      alt="${t.title}"
       />
-      <p class="p1"> - ${e.title_english} </p>
-      <p class="p2"> - ${e.title} </p>
-    </li>`:`<li class="li-anime js_liAnime favourites" data-hook=${e.mal_id}>
+      <p class="p1"> - ${t.title_english} </p>
+      <p class="p2"> - ${t.title} </p>
+    </li>`:`<li class="li-anime js_liAnime favourites" data-hook=${t.mal_id}>
       <img class="anime-picture" 
-      src=${e.images.jpg.image_url}
-      alt="${e.title}"
+      src=${t.images.jpg.image_url}
+      alt="${t.title}"
       />
-      <p class="p1"> - ${e.title_english} </p>
-      <p class="p2"> - ${e.title} </p>
-    </li>`}function v(){let e="";for(const t of o)e+=q(t);L.innerHTML=e}function k(e){return`<li class="li-anime2 js_liAnime" data-hook=${e.mal_id}>
+      <p class="p1"> - ${t.title_english} </p>
+      <p class="p2"> - ${t.title} </p>
+    </li>`}function g(){let t="";for(const e of d)t+=T(e);S.innerHTML=t}function _(t){return`<li class="li-anime2 js_liAnime" data-hook=${t.mal_id}>
       <img class="anime-picture2" 
-      src=${e.images.jpg.image_url}
-      alt="${e.title}"
+      src=${t.images.jpg.image_url}
+      alt="${t.title}"
       />
-      <p class="p2-fav"> - ${e.title} </p>
-      <img class="delete js_deleteImg" data-hook=${e.mal_id} src="./images/marca-de-la-cruz.png" alt="cruz para eliminar favorito" />
-    </li>`}function d(e){let t="";for(e of l)t+=k(e);r.innerHTML=t,m()}function E(e){e.preventDefault();const t=h.value;fetch(`https://api.jikan.moe/v4/anime?q=${t}`).then(i=>i.json()).then(i=>{o=i.data,v(),document.querySelectorAll(".js_liAnime").forEach(c=>{c.addEventListener("click",s=>{const a=s.currentTarget;a.classList.toggle("favourites");const u=a.dataset.hook,p=l.findIndex(n=>n.mal_id===parseInt(u));if(p===-1){const n=o.find(j=>j.mal_id===parseInt(u));l.push(n),localStorage.setItem("favourites",JSON.stringify(l));const $=k(n);r.innerHTML+=$,m()}else l.splice(p,1),d()})})})}function x(){l=[],h.value="",localStorage.removeItem("favourites"),v(),location.reload()}I.addEventListener("click",E);C.addEventListener("click",x);const f=JSON.parse(localStorage.getItem("favourites"));f!==null&&(l=f,d());function m(){document.querySelectorAll(".js_deleteImg").forEach(t=>{t.addEventListener("click",i=>{const c=i.currentTarget.dataset.hook,s=l.findIndex(a=>a.mal_id===parseInt(c));s!==-1&&(l.splice(s,1),localStorage.setItem("favourites",JSON.stringify(l)),d(),m())})})}
+      <p class="p2-fav"> - ${t.title} </p>
+      <img class="delete js_deleteImg" data-hook=${t.mal_id} src="./images/marca-de-la-cruz.png" alt="cruz para eliminar favorito" />
+    </li>`}function m(t){let e="";for(t of i)e+=_(t);o.innerHTML=e,u()}function E(t){return fetch(`https://api.jikan.moe/v4/anime?q=${t}`).then(e=>e.json()).then(e=>e.data)}function F(){document.querySelectorAll(".js_liAnime").forEach(e=>{e.addEventListener("click",l=>{const n=l.currentTarget;n.classList.toggle("favourites");const c=n.dataset.hook,a=i.findIndex(s=>s.mal_id===parseInt(c));if(a===-1){const s=d.find(k=>k.mal_id===parseInt(c));i.push(s),localStorage.setItem("favourites",JSON.stringify(i));const $=_(s);o.innerHTML+=$,u()}else i.splice(a,1),m()})})}function v(t){t.preventDefault();const e=r.value.trim();E(e).then(l=>{d=l,g(),F()})}function q(){i=[],r.value="",localStorage.removeItem("favourites"),g(),location.reload()}j.addEventListener("click",v);L.addEventListener("click",q);r.addEventListener("keydown",t=>{t.key==="Enter"&&v(t)});const p=JSON.parse(localStorage.getItem("favourites"));p!==null&&(i=p,m());function u(){document.querySelectorAll(".js_deleteImg").forEach(e=>{e.addEventListener("click",l=>{const c=l.currentTarget.dataset.hook,a=i.findIndex(s=>s.mal_id===parseInt(c));a!==-1&&(i.splice(a,1),localStorage.setItem("favourites",JSON.stringify(i)),m(),u())})})}
 //# sourceMappingURL=main.js.map
